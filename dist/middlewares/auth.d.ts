ced71f59507440e84c9e034fe204f92720d17695
reves <ricardo.reves@gmail.com>
2019-05-02 18:41:10 +0200

    rm .temp folder

--- dist/middlewares/auth.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const authMiddleware: (req: Request & {
-    tokenContent?: any;
-}, res: Response, next: NextFunction) => import("express-serve-static-core").Response;
-export declare const checkToken: (token: any) => false | object;
