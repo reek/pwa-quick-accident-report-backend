9747bf9cef6bc5516f5605afc6cb51f1c74a7d1e
reves <ricardo.reves@gmail.com>
2019-05-02 13:58:26 +0200

    rm .temp folder

--- dist/providers/sendgrid/sendgrid.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="request" />
-export declare const sendMailWithNewPassword: (to: string, newPassword: string) => Promise<[import("request").Response, {}]>;
-export declare const sendMailResgisterOK: (to: string) => Promise<[import("request").Response, {}]>;
-export declare const sendTestMail: () => void;
--- dist/routes/handlers/personal.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-export declare const getUserPersonalHandler: (req: Request & {
-    tokenContent?: any;
-}, res: Response) => void;
-export declare const updateUserPersonalHandler: (req: Request & {
-    tokenContent?: any;
-}, res: Response) => void;
