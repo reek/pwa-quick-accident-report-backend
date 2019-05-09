0b180b2391d47d22d2189067049abf14ce6ea93e
reves <ricardo.reves@gmail.com>
2019-05-09 22:39:14 +0200

    rm .temp folder

--- dist/providers/imgur/imgur.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const uploadImageBase64Asnc: (base64: string) => Promise<any>;
-export declare const uploadImagesBase64: (images: any[]) => any;
-export declare const uploadImagesBase64Async: (images: any[]) => Promise<any>;
-export declare const uploadImagesUrlAsync: (images: any[]) => Promise<any>;
