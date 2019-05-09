18ded2a654222240083953ff2745cb4084cf6dc5
reves <ricardo.reves@gmail.com>
2019-05-09 22:35:10 +0200

    improve upload functions

--- src/providers/imgur/imgur.ts
+++ src/providers/imgur/imgur.ts
@@ -6,10 +6,10 @@ import * as imgur from 'imgur';
 imgur.setClientId(process.env.IMGUR_CLIENT_ID);
 imgur.setAPIUrl(process.env.IMGUR_API_URL)
 
-/* export const uploadImageBase64 = async (base64: string) => {
+export const uploadImageBase64Asnc = async (base64: string) => {
     const data = base64.split(";base64,").pop()
-    return await imgur.uploadBase64(data).then(_ => _.data.link);
-} */
+    return await imgur.uploadBase64(data).then(res => res.data.link).catch((err) => err.message);
+}
 
 export const uploadImagesBase64 = (images: any[]) => {
     const imgs = images.map(item => item.imageUrl.split(";base64,").pop())
@@ -22,13 +22,7 @@ export const uploadImagesBase64 = (images: any[]) => {
 }
 
 export const uploadImagesBase64Async = async (images: any[]) => {
-    const imgs = images.map(item => item.imageUrl.split(";base64,").pop())
-    return await imgur.uploadImages(imgs, "Base64")
-        .then((res) => res.map(data => data.link))
-        .catch((err) => {
-            console.error(err.message);
-            return false
-        });
+    return await uploadImagesBase64(images)
 }
 
 export const uploadImagesUrlAsync = async (images: any[]) => {
